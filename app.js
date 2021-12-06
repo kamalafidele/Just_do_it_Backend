bdb18cc680f8bf166705d1cc355f7ab02b51a040
Kamara <74622768+kamalafidele@users.noreply.github.com>
2021-12-06 18:45:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function(req,res){
   });
 
 //RUNNING FOR EVERY DAY 
-var job=new cronJob('10 13 * * *',function(){
+var job=new cronJob('0 19 * * *',function(){
   sendDailyEmail();
 },null,true,'Africa/Kigali');
 
